bc9a5a8d0bddab7765e80d421aac920ee059e6e3
Luan Cassiano Lima de Souza <luan.cassiano0@gmail.com>
2019-06-19 11:44:49 -0300

    Model Motorista

--- rentgo-backend/src/app/controllers/userController.js
+++ rentgo-backend/src/app/controllers/userController.js
@@ -3,7 +3,6 @@ const User = require('../models/user');
 class UserController {
 
     async register(req, res) {
-        console.log('ola mundo')
         const { email } = req.body;
 
         try {
@@ -35,13 +34,10 @@ class UserController {
     async authentication(req, res) {
         try {
             const { email, password } = req.body;
-            console.log(email, password)
             
             const user = await User.findOne({
                 email
             });
-
-            console.log(user)
 
             if(!user) {
                 return res.status(400).send({
