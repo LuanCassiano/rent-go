8c27daace336d4d09be67772c26138e1b0db4916
Luan Cassiano Lima de Souza <luan.cassiano0@gmail.com>
2019-08-19 22:01:23 -0300

    Progresso no desenvolvimento

--- rentgo-frontend/rentgo/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import 'bootstrap/dist/js/bootstrap.min.js';
-import { BrowserRouter as Router } from 'react-router-dom'
-
-ReactDOM.render(<Router><App /></Router>, document.getElementById('root'));
--- rentgo-frontend/rentgo/src/pages/Passengers/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-// import { Container } from './styles';
-
-export default function Passengers() {
-  return (
-    <div />
-  );
-}
